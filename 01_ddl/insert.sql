6562595066b13cfc6ee59e7020d125155a4b8072
Zaur <zaurik87@gmail.com>
2026-02-25 23:22:04 +0400

    autoincrement

--- 01_ddl/insert.sql
+++ 01_ddl/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO company (id, name, date)
+INSERT INTO company (id, name, founded_date)
 VALUES (1, 'Google', '2001-01-01'),
        (2, 'Apple', '2002-10-29'),
        (3, 'Facebook', '1998-09-13');
